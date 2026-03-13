c71f276c96d2d6d9f15f38e4db7510ee54ac6dec
Pablo López de Lerena <pablolp257@gmail.com>
2026-03-13 20:25:22 +0100

    restore files

--- src/pages/About.tsx
+++
@@ -1,9 +0,0 @@
-export default function About(){
-    //Logica si hubiera
-    return(
-        <div>
-            <h1>Sobre mi</h1>
-            <p>Esta es una nueva ruta</p>
-        </div>
-    )
-}
